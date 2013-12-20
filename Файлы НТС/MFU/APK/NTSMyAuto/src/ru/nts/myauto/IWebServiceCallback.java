f40098de6be95145b072e95f8abb88a5b82d4454
awersatos <sanya.gordejchik2gmail.com>
2013-12-20 14:16:08 +0700

    12

--- Файлы НТС/MFU/APK/NTSMyAuto/src/ru/nts/myauto/IWebServiceCallback.java
+++
@@ -1,6 +0,0 @@
-package ru.nts.myauto;
-
-public interface IWebServiceCallback {
-	void onSuccess(String callType, String callID, String result);
-	void onFault(String callType, String callID, String reason);
-}
